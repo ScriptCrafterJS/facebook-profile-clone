3ecc20eab5dfc02a59af86ce24e33af690e72b25
Motasem Ali <motasemali0000@gmail.com>
2024-05-12 02:07:03 +0300

    Gulp for compiling, minifiying, and browserSync
    - building a task to start a server with notification at the bottom
    - building a task to take scss to css
    - building a task to reload the server whenever a file is saved

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass")); //load gulp-sass with the Dart Sass compiler
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const browserSync = require("browser-sync").create();
+const cssnano = require("cssnano");
+
+// COMPILE SASS
+
+function compileToCSS() {
+  return src("scss/style.scss", { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(dest("dist", { sourcemaps: "." }));
+}
+
+function startServer(cb) {
+  browserSync.init({
+    server: {
+      baseDir: ".",
+    },
+    notify: {
+      styles: {
+        top: "auto",
+        bottom: "0",
+      },
+    },
+  });
+  cb();
+}
+
+function reloadBrowser(cb) {
+  browserSync.reload();
+  cb();
+}
+
+function watchTask() {
+  watch("*.html", reloadBrowser);
+  watch("scss/*.scss", series(compileToCSS, reloadBrowser));
+}
+
+exports.default = series(compileToCSS, startServer, watchTask);
